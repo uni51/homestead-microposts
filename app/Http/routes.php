55240a9091ffbc9068f571c149a200539ee4a86d
vagrant <vagrant@homestead>
2018-07-06 04:55:19 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
