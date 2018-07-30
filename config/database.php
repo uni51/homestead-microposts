88d6aba122b13d3820d8dd27af36e954c2ae2ed8
uni51 <uni51@users.noreply.github.com>
2018-07-30 14:10:26 +0900

    modify Circle CI

--- config/database.php
+++ config/database.php
@@ -91,7 +91,7 @@ return [
                 'host' => '127.0.0.1',
                 'port' => '3306',
                 'database' => 'circle_test',
-                'username' => 'ubuntu',
+                'username' => 'root',
                 'password' => '',
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
