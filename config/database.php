1bceb12a26597b793707e83f1c00c5ed86c74854
uni51 <uni51@users.noreply.github.com>
2018-07-30 13:43:52 +0900

    add Circle CI

--- config/database.php
+++ config/database.php
@@ -85,6 +85,20 @@ return [
             'prefix'   => '',
         ],
 
+        'connections' => [
+            'circle_test' => [
+                'driver' => 'mysql',
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'database' => 'circle_test',
+                'username' => 'ubuntu',
+                'password' => '',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+        ],
     ],
 
     /*
